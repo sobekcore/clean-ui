5d23ce1582cb812bc768642eb98db2b29c82c485
sobekcore <sobekcore@gmail.com>
2021-06-17 00:43:33 +0200

    Basic home page

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,46 +1,62 @@
 <template>
-  <nav class="navbar">
-    <ul class="navbar-list">
-      <li class="logo">Clean UI</li>
-      <li class="navbar-item">Home</li>
-      <li class="navbar-item">About</li>
-      <li class="navbar-item">Components</li>
-    </ul>
-  </nav>
+  <div class="margin">
+    <nav class="navbar">
+      <ul class="navbar-list">
+        <li class="logo">
+          <img class="logo-img" src="~/assets/images/logo.png" />
+          Clean UI
+        </li>
+        <li class="navbar-item">Home</li>
+        <li class="navbar-item">About</li>
+        <li class="navbar-item">Components</li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
-<style scoped>
-.navbar {
-  background: white;
-  border-bottom: 2px solid #eaeaec;
-  box-shadow: 0 1px 3px rgb(0 0 0 / 10%);
-  z-index: 99;
-}
-
-.navbar-list {
-  display: inline-flex;
-  list-style: none;
+<style scoped lang="scss">
+.margin {
+  height: $navbar-margin;
   width: 100%;
-  padding: 0;
-  margin: 0;
-}
 
-.navbar-item {
-  padding: 18px 22px;
-  transition: background 0.15s;
-}
+  .navbar {
+    position: fixed;
+    background: white;
+    border-bottom: 2px solid $light-gray;
+    box-shadow: 0 1px 3px rgb(0 0 0 / 10%);
+    width: 100%;
+    z-index: 99;
 
-.navbar-item:hover {
-  background: #eaeaec;
-  cursor: pointer;
-  transition: background 0s;
-}
+    .navbar-list {
+      display: inline-flex;
+      list-style: none;
+      padding: 0;
+      margin: 0;
 
-.logo {
-  display: flex;
-  font-size: 24px;
-  align-items: center;
-  font-family: "IBM Plex Sans", sans-serif;
-  padding: 0 18px;
+      .navbar-item {
+        padding: 18px 22px;
+        transition: background 0.15s;
+
+        &:hover {
+          background: $light-gray;
+          cursor: pointer;
+          transition: background 0s;
+        }
+      }
+
+      .logo {
+        display: flex;
+        font-size: 24px;
+        align-items: center;
+        font-family: "IBM Plex Sans", sans-serif;
+        padding: 0 18px;
+
+        .logo-img {
+          margin-right: 7px;
+          width: 32px;
+        }
+      }
+    }
+  }
 }
 </style>
